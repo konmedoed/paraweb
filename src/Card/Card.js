8708ba4614015f72a58d4fb2b0aeddd43009fb2a
Kirill Kostin <79797389+konmedoed@users.noreply.github.com>
2022-07-02 18:08:32 +0300

    02.07 start of work

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -10,7 +10,7 @@ export default function Card({date, title, text, author}){
             <div className='card__wrapper'>
                 <p className='card__date text'>{dateObj.getDate()} {dateObj.toLocaleString('ru', { month: 'long' })} {dateObj.getFullYear()}</p>
                 <h3 className='card__title text'>{title}</h3>
-                <article className='card__text text text_normal'>{text}</article>
+                <p className='card__text text text_normal'>{text}</p>
             </div>
             <div className='card__author'>
                 <p className='card__name text text_small'>{author}</p>
